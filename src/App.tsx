2c68661ec9dfe6f9c9da4b31f0d1d51da3a9769d
joshsj89 <joshsj89@gmail.com>
2025-04-16 23:39:35 -0700

    Add barebone popup background

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const year = new Date().getFullYear();
+
+    return (
+        <div className="App">
+            <div className="title">
+                <h1>ClassTrack</h1>
+            </div>
+            <div className="main">
+
+            </div>
+            <div className="footer">
+            <p>&copy; {year} ClassTrack. All rights reserved.</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
