ebf4ff0240da88727d69749ef7e9d3a091a1c5e4
joshsj89 <joshsj89@gmail.com>
2025-04-19 16:13:20 -0700

    Update Toggle.tsx

--- src/Toggle.tsx
+++ src/Toggle.tsx
@@ -3,17 +3,16 @@ import styles from "./Toggle.module.css";
 
 function Toggle({ label, onChange, backgroundColor }: { label: string; onChange: (checked: boolean) => void, backgroundColor?: string }) {
     const sliderRef = useRef<HTMLSpanElement>(null);
-    const inputRef = useRef<HTMLInputElement>(null);
 
     return (
         <div className={styles["toggle-row"]}>
             <label className={styles["switch"]}>
                 <input 
-                    type="checkbox" 
-                    ref={inputRef} 
+                    type="checkbox"
                     onChange={(e) => {
                         onChange(e.target.checked);
 
+                        // Change the background color of the slider based on the checked state
                         if (sliderRef.current) {
                             sliderRef.current.style.backgroundColor = e.target.checked ? backgroundColor || "#FF5652" : "#CCC";
                         }
